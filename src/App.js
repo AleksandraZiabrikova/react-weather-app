3cceb4d6bb7dfddf9f16e3db5a0b18e7d1bded0e
AleksandraZiabrikova <109486703+AleksandraZiabrikova@users.noreply.github.com>
2022-10-30 15:11:04 +0100

    added github repo url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="React weather app">React weather app</div>;
+  return (
+    <div className="React weather app">
+      <h1>React weather app</h1>
+      <footer>
+        This project is coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/aleksandra-ziabrikova-19514a65/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Aleksandra Ziabrikova
+        </a>{" "}
+        and is available at{" "}
+        <a
+          href="https://github.com/AleksandraZiabrikova/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>
+      </footer>
+    </div>
+  );
 }
