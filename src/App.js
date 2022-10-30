d153ccbab73e5117b4c8a0026b29d6c280f99336
AleksandraZiabrikova <109486703+AleksandraZiabrikova@users.noreply.github.com>
2022-10-30 15:29:12 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,25 +3,28 @@ import "./App.css";
 export default function App() {
   return (
     <div className="React weather app">
-      <h1>React weather app</h1>
-      <footer>
-        This project is coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/aleksandra-ziabrikova-19514a65/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Aleksandra Ziabrikova
-        </a>{" "}
-        and is available at{" "}
-        <a
-          href="https://github.com/AleksandraZiabrikova/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>React weather app</h1>
+        <div className="btn btn-primary">Hello</div>
+        <footer>
+          This project is coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/aleksandra-ziabrikova-19514a65/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Aleksandra Ziabrikova
+          </a>{" "}
+          and is available at{" "}
+          <a
+            href="https://github.com/AleksandraZiabrikova/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
