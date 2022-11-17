405929da159904aeca2d8a1d2ad8aefc55e107da
AleksandraZiabrikova <109486703+AleksandraZiabrikova@users.noreply.github.com>
2022-11-17 20:40:02 +0100

    refactoring - move code to a separate component

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./Weather.css";
 import axios from "axios";
-import FormattedDate from "./FormattedDate";
+import WeatherInfo from "./WeatherInfo";
 
 export default function Weather() {
   const [weatherData, setWeatherData] = useState({ ready: false });
@@ -40,31 +40,7 @@ export default function Weather() {
             </div>
           </div>
         </form>
-        <h1>{weatherData.city}</h1>
-        <ul>
-          <li>
-            <FormattedDate date={weatherData.date} />
-          </li>
-          <li className="text-capitalize">{weatherData.description}</li>
-        </ul>
-        <div className="row mt-3">
-          <div className="col-6">
-            <img src={weatherData.iconUrl} alt={weatherData.iconDescription} />
-
-            <span className="temperature">
-              {Math.round(weatherData.temperature)}
-            </span>
-
-            <span className="unit">°C</span>
-          </div>
-
-          <div className="col-6">
-            <ul>
-              <li>Humidity: {weatherData.humidity}%</li>
-              <li>Wind: {Math.round(weatherData.wind)}m/s</li>
-            </ul>
-          </div>
-        </div>
+        <WeatherInfo data={weatherData} />
       </div>
     );
   } else {
