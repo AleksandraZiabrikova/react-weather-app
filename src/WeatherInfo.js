66e7b699d248b32a39093bbfdbf6dca9f1f1e557
AleksandraZiabrikova <109486703+AleksandraZiabrikova@users.noreply.github.com>
2022-11-19 20:04:30 +0100

    Add animated icon

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
   return (
@@ -13,15 +14,18 @@ export default function WeatherInfo(props) {
       </ul>
       <div className="row mt-3">
         <div className="col-6">
-          <img src={props.data.iconUrl} alt={props.data.iconDescription} />
+          <div className="d-flex">
+            <div>
+              <WeatherIcon code={props.data.icon} />
+            </div>
 
-          <span className="temperature">
-            {Math.round(props.data.temperature)}
-          </span>
+            <span className="temperature">
+              {Math.round(props.data.temperature)}
+            </span>
 
-          <span className="unit">°C</span>
+            <span className="unit">°C</span>
+          </div>
         </div>
-
         <div className="col-6">
           <ul>
             <li>Humidity: {props.data.humidity}%</li>
